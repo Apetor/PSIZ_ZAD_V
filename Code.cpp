a215e35b70be04de5778fd15185babdcbf00f422
Apetor <102623708+Apetor@users.noreply.github.com>
2022-06-18 21:28:07 +0200

    Create Code.cpp

---
+++ Code.cpp
@@ -0,0 +1,22 @@
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include <string_view>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  const char* file_path = "test.bmp";
+  if (argc >= 2) {
+    file_path = argv[1];
+  }
+
+  ifstream file(file_path);
+  cout << "\"bitmap_file_header\": ";
+  bitmap_file_header::read(file).format(cout, "", "  ") << endl;
+  cout << "\"dib_bitmap_info_header\": ";
+  dib_bitmap_info_header::read(file).format(cout, "", "  ") << endl;
+  return 0;
+}
